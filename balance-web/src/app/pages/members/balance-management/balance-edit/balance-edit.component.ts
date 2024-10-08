9a95768976220fefce2c0d31a8aad09c01ae56bd
HeinThantAung <heinthant098@gmail.com>
2024-10-08 13:06:28 +0630

    feat:add edit view

--- balance-web/src/app/pages/members/balance-management/balance-edit/balance-edit.component.ts
+++ balance-web/src/app/pages/members/balance-management/balance-edit/balance-edit.component.ts
@@ -1,18 +1,71 @@
 import { Component, computed, input } from '@angular/core';
 import { WidgetsModule } from '../../../../widgets/widgets.module';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-balance-edit',
   standalone: true,
-  imports: [WidgetsModule],
+  imports: [WidgetsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './balance-edit.component.html',
   styles: ``
 })
 export class BalanceEditComponent {
+
+  form: FormGroup
   type = input.required<string>();
   id = input<number>();
 
   icon = computed(() => this.type() == 'Credit' ? 'bi-wallet' : 'bi-cart');
   subTitle = computed(() => `${this.type()} Entry`);
   title = computed(() => this.id() == undefined ? this.subTitle() : `Edit ${this.subTitle()}`);
+
+  constructor(private builder: FormBuilder) {
+    this.form = builder.group({
+      issueAt: ['', Validators.required],
+      ledgerCode: ['', Validators.required],
+      particular: '',
+      items: builder.array([])
+    });
+
+    this.addItem();
+  }
+
+  get items(): FormArray {
+    return this.form.get('items') as FormArray;
+  }
+
+  addItem() {
+    this.items.push(this.builder.group({
+      item: ['', Validators.required],
+      unitPrice: [0, Validators.required],
+      quantity: [0, Validators.required]
+    }));
+  }
+
+  removeItem(index: number) {
+    this.items.removeAt(index);
+
+    if (this.items.controls.length == 0) {
+      this.addItem();
+    }
+  }
+
+  getItemTotal(index: number) {
+    const itemGroup = this.items.controls[index] as FormGroup;
+    const unitPrice = itemGroup.get('unitPrice')?.value || 0;
+    const quantity = itemGroup.get('quantity')?.value || 0;
+    return unitPrice * quantity;
+  }
+
+  getTotal() {
+    let total = 0;
+    for (const item of this.items.controls) {
+      const itemGroup = item as FormGroup;
+      const unitPrice = itemGroup.get('unitPrice')?.value || 0;
+      const quantity = itemGroup.get('quantity')?.value || 0;
+      total += (unitPrice * quantity);
+    }
+    return total;
+  }
 }
